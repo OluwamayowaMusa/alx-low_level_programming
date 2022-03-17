9f328aa6f995c2a9a03c59c087eae82c7abfd6d6
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-17 01:54:37 +0000

    Print string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stido.h>
+
+/**
+ * main - Print string using printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0)
+}
