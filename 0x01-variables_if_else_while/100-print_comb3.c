d82a014a074c4a53e62c2a7790b3aadf9dae5b66
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-19 21:09:27 +0000

    Print different combination of digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Prints different combinations of two digits
+ * Return:0
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 49;
+	while (i < 57)
+	{
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(44);
+			putchar(32);
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar(10);
+	return (0);
+}
