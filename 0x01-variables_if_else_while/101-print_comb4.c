00135ac58159cec997f0f20574c8c340343a1c4b
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-19 21:40:31 +0000

    Print all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print all possible combination of three digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 48;
+	j = 49;
+	k = 50;
+	while (i < 55)
+	{
+		while (j  < 57)
+		{
+			while (k < 58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 54)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
