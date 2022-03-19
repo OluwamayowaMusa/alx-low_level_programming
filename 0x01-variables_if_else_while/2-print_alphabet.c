774f12ee4b014d771dcea7a0b2a70efa81f2b9e2
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-19 18:42:35 +0000

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i += 1
+	}
+	putchar(10);
+	return (10);
+}
