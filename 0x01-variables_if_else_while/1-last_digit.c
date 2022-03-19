acfe647a6f4de4052fb2a4ed030beff3406a70a4
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-19 18:27:36 +0000

    Determine if last digit is greater than 5 or less than 6 and is not 0 or is zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -36,7 +36,7 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		i = n % 10 - 10;
+		i = n % 10;
 		if ((i < 6) && (i != 0))
 		{
 			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
