489fe1a9f422c759d92712e9a7923cfe8795e519
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-19 23:02:45 +0000

    Print all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print all possible combinations of two two-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	i = 48;
+	j = 48;
+	k = 48;
+	l = 48;
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			while (k < 58)
+			{
+				while (l < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(l);
+					putchar(k);
+					putchar(44);
+					l++;
+
+				}
+				k++;
+				l = 48;
+			}
+			j++;
+			k = 48;
+			l = j + 1;
+		}
+		i++;
+		j = 48;
+		k = i;
+		l = j + 1;
+	}
+	putchar(10);
+	return (0);
+}
