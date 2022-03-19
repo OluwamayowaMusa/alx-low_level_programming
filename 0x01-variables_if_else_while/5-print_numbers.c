242bcadf3feebf1dd95baa018ce74d183809ae22
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-19 19:17:51 +0000

    Print all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print all single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
