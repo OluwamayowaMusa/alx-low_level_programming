873513dddc16bf62164535c662e0314c71283053
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-22 10:49:56 +0000

    Print the lowercase alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point, no arguments
+ * Print the lower case letters 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	i = 0;
+	j = 97;
+	while (i < 10)
+	{
+		while (j < 123)
+		{
+			_putchar(j);
+			j++;
+		}
+		i++;
+		j = 97;
+	}
+	_putchar(10);
+}
