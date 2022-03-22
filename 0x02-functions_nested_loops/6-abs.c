d918dc65e91d4741bc9f54087633283d5575c5a1
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-22 13:29:10 +0000

    Absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Print absolute number
+ * @c: Number to be checked
+ * Return:Absolute number
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return(-c);
+	}
+}
