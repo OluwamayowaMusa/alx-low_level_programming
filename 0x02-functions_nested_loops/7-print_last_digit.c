996668fd8575754ed8a6da44e27a162f04afa2e3
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-22 13:39:18 +0000

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit -Print last digit of a number
+ * @c: Number to be checked
+ * Return: Value of the last digit
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
