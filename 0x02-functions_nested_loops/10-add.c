1436541c4358933cd41b128a256fd3d71b9622a5
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-23 14:21:16 +0000

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers
+ * @a - First number
+ * @b - Second number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
