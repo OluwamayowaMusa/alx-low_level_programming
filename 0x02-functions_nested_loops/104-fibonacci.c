e6180026938c796c45033ffb6d9485bbdee742cf
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-23 17:12:49 +0000

    First 98 fibonacci number

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print the first fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	 unsigned long int i, j, tmp;
+	int count;
+
+	i = j = count = 1;
+	while (count < 99)
+	{
+		printf("%lu", j);
+		tmp = j;
+		j += i;
+		i = tmp;
+		if (count != 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		count++;
+	}
+	putchar(10);
+	return (0);
+}
