df2db93a3757134b130261ba77b36a1394e05ec4
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-23 13:39:11 +0000

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 					_putchar(32);
 				}
 			}
-			else if ((((i > 1) && k == 5) || (k > 6 && k < 10)) || (i == 6 && k == 6))
+			else if ((i > 1) && ((k == 5) && (k > 6 && k < 10)) || (i == 6 && k == 6))
 			{
 				_putchar(k + '0');
 				if (j != 9)
