3b5fd48f96765433834de83fab1233c7f515dcd8
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-23 17:06:11 +0000

    Sum of even termed fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * sum of the even valued fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	 unsigned long int i, j, tmp, sum;
+
+	i = j = 1;
+	sum = 0;
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			sum += j
+		tmp = j;
+		j += i;
+		i = tmp;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
