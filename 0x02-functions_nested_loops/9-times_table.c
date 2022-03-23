e16eb8a8b015f0a69554ebcd24875fb955cabdcf
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-23 12:06:41 +0000

    Times Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,19 +5,31 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+	int i, j, k, l, m;
 
 	i = j = 0;
-	while (i < 9)
+	while (i < 10)
 	{
-		while (j < 9)
+		while (j < 10)
 		{
 			k = i * j;
-			_putchar(k + '0');
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
-			j++;
+			if (k > 9)
+			{
+				l = k / 10;
+				m = k % 10;
+				_putchar(l + '0');
+				_putchar(m + '0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+			else
+			{
+				_putchar(k + '0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
 		}
 		_putchar(10);
 		i++;
