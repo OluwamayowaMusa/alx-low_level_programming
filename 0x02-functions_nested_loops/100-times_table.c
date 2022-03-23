bdb1870d3c8c1b9ce03aab9621f95a4ff97a6df1
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-23 16:00:24 +0000

    Print N times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * print_times_table - Print the n times table
+ * @n: The number of time table
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	i = j = 0;
+	if (n > 0 && n < 15)
+	{
+		while (i < (n + 1))
+		{
+			while (j < (n + 1))
+			{
+				k = i * j;
+				if  (k > 9 && k < 100)
+				{
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+					if (j != n)
+					{
+						_putchar(44);
+						_putchar(32);
+						_putchar(32);
+					}
+				}
+				else if (k > 99)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k % 100) / 10) + '0');
+					_putchar((k % 10) + '0');
+					if (j != n)
+					{
+						_putchar(44);
+						_putchar(32);
+					}
+				}
+				else
+				{
+					_putchar(k + '0');
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
+				}
+				j++;
+			}
+			i++;
+			j = 0;
+		}
+	}
+}
