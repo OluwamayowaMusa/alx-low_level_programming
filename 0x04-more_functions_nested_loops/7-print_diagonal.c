c7a2121d0038ba768b850bf931d8f957ee811d2d
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-24 16:45:31 +0000

    Print Diagonal N times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print diagonal line
+ * @n: Number of times to print line
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	i = j = 0;
+	if (n < 1)
+	{
+		_putchat(10);
+	}
+	else
+	{
+		while (i < n)
+		{
+			while (j < i)
+			{
+				_putchar(32);
+				j++
+			}
+			_putchar(92);
+			_putchar(10);
+			i++;
+			j = 0;
+		}
+		_putchar(10);
+	}
+}
