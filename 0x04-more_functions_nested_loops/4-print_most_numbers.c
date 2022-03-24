03de3a7c5ebb3ff94af41d888ae78df280b5fe70
OluwamayowaMusa <oluwamayowamusa01@gamil.com>
2022-03-24 14:26:22 +0000

    Print numbers from 0 to 99 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print the numbers 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i != 50 || i != 52)
+			_putchar(i);
+		i++;
+	}
+}
