9d9b4a49547edce856c89e63b197de077b1b20d1
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-03-26 16:33:47 +0000

    Print Number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,71 +1,19 @@
 #include "main.h"
 
 /**
- * power - Returns the the result of base to the power of expoonet
- * @base: Base
- * @exp: Exponent
- * Return: The result
- */
-int power(int base, int exp)
-{
-	int i, result;
-
-	i = 0;
-	result = 1;
-	while (i < exp)
-	{
-		result *= base;
-		i++;
-	}
-	return (result);
-}
-
-/**
  * print_number - Prints an integer
  * @n: Number to be printed
  */
 void print_number(int n)
 {
-	int j, k, l, m;
+	unsigned int i = n;
 
-	k = 0;
-	if (n > -1)
-	{
-		j = n;
-		while (j > 9)
-		{
-			j /= 10;
-			k++;
-		}
-		l = power(10, k);
-		_putchar((n / l) + '0');
-		while (k > 1)
-		{
-			l = power(10, k);
-			m = power(10, (k - 1));
-			_putchar(((n % l) / m) + '0');
-			k--;
-		}
-		_putchar((n % 10) + '0');
-	}
-	else
+	if (i < 0)
 	{
 		_putchar(45);
-		n = n * -1;
-		j = n;
-		while (j > 9)
-		{
-			j /= 10;
-			k++;
-		}
-		l = power(10, k);
-		while (k > 1)
-		{
-			l = power(10, k);
-			m = power(10, (k - 1));
-			_putchar(((n % l) / m) + '0');
-			k--;
-		}
-		_putchar((n % 10) + '0');
+		i = -i;
 	}
+	if ((i / 10) > 0)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
 }
