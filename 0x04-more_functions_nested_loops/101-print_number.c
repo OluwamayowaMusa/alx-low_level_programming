14eb1ae4f365c567558e24fde3544440ae2b700a
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-03-26 13:18:53 +0000

    Print an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - Print an integer
+ * @n: Number to be printed
+ */
+void print_number(int n)
+{
+	if (n > -1 && n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n < 100)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n > 99 && n < 1000)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n % 100) / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
