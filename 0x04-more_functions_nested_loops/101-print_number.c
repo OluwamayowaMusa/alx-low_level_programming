bc5f4f75cbd3db7bd3363251b69708c5042adaf0
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-03-26 13:29:45 +0000

    Print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -21,4 +21,11 @@ void print_number(int n)
 		_putchar(((n % 100) / 10) + '0');
 		_putchar((n % 10) + '0');
 	}
+	else if (n > 999 && n < 10000)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n % 1000) / 100) + '0');
+		_putchar(((n % 100) / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
 }
