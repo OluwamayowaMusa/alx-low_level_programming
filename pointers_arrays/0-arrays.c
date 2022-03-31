c242b16beef78d8e1fc1ba5e0e9268410f11bbf9
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-03-31 12:22:44 +0000

    Arrays

---
+++ pointers_arrays/0-arrays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Accessing the elements of an array
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a[2];
+
+	a[0] = 1;
+	a[1] = 2;
+	printf("Value and address of 'a[0]': %d %p\n", a[0], &(a[0]));
+	printf("Value and address of 'a[1]': %d %p\n", a[1], &(a[1]));
+
+	return (0);
+}
