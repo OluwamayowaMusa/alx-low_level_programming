506bcc697418c285defc51226232ae4ee53df0af
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-03-31 14:57:03 +0000

    Return the length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of the string passed
+ * @s: String to be passed
+ * Return: the length of string
+ */
+int _strlen(char *s)
+{
+	char c;
+	int i = 0;
+
+	while c != '\0'
+	{
+		c = *(s + i);
+		i++;
+	}
+	return (i);
+}
