cce7c4ed584ccce5762063e98e70e22dba04b230
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-03-31 16:04:40 +0000

    Print String in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Print string in reverse order
+ * @s: String Passed
+ */
+void print_rev(char *s)
+{
+	char c;
+	int i, h;
+
+	c = ' ';
+	i = 0;
+	while (c != '\0')
+	{
+		c = *(s + i);
+		h = i++;
+	}
+	while (h > -1)
+	{
+		c = *(s + h);
+		_putchar(c);
+		h--;
+	}
+	_putchar(10);
+}
