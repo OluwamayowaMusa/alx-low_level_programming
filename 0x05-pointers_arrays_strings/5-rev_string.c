77133b8dfcf2d135731ba106287857ffde39c8db
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-01 13:28:19 +0000

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,5 +6,23 @@
  */
 void rev_string(char *s)
 {
+	char *tmp, c;
+	int i, h;
+
+	tmp = s;
+	i = 0;
+	while (c != '\0')
+	{
+		c = *(str + i);
+		h = i++;
+	}
+	i = 0;
+	h--;
+	while (h > -1)
+	{
+		*(s + i) = *(tmp + h);
+		i++;
+		h--;
+	}
 
 }
