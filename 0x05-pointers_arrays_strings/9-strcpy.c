7a040151eedd60db272c5f5103cfc8b7c920fe4a
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-01 18:55:49 +0000

    Copies string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,19 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int k, h;
+	char c;
 
+	k = 0;
+	while (c != '\0')
+	{
+		c = *(src + k);
+		h = k++;
+	}
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
+	for ( ; i < h; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
