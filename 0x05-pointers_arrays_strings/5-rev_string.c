66ccb067d8d5a06512a7685eeb2076cb3ddce48d
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-01 11:26:57 +0000

    Print Every other character of a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string
+ * @s: Sting passed
+ */
+void rev_string(char *s)
+{
+
+}
---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - Print every other character of a string 
+ * @str: String passed
+ */
+void puts2(char *str)
+{
+	int i, h;
+
+	i = _strlen(str);
+	for (h = 0; h <= i; h += 2;)
+		_putchar(*(str + h));
+	_putchar(10);
+}
