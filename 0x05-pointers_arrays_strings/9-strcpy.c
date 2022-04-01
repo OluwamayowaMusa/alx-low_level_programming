688042f16ee3a552a798ad2861b902146c7f81de
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-01 15:52:34 +0000

    Copies string to a pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string to a pointer
+ * @dest: the pointer string is copied to
+ * @src: the string to be copied
+ * Return: Pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
