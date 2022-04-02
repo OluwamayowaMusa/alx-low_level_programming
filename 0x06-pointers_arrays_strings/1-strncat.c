a291f3c79c3523abc614931d608afe1122ccb769
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-02 22:43:55 +0000

    Conacatenate two string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -27,10 +27,11 @@ int _strlen(char *s)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len_1;
+	int len_1, i;
 	char *str;
 
 	len_1 = _strlen(dest);
+	len_2 = _strlen(src);
 	for (i = 0; i < n; i++)
 		dest[len_1 + i] = src[i];
 	dest[len_1 + i] = '\0';
