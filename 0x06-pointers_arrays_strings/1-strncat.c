01ec8ef19c9307cbac87d8f46025cfcdae116641
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-03 10:44:13 +0000

    I

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen1 - Length of string
+ * @s: String passed
+ * Return: Length
+ */
+int _strlen1(char *s)
+{
+	char c = ' ';
+	int i, h;
+
+	while (c != '\0')
+	{
+		c = *(s + i);
+		h = i++;
+	}
+	return (h);
+}
+
+/**
  * _strncat - Concatenates two strings
  * @dest: First string
  * @src: Second string
@@ -9,18 +27,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len_1 = 0, len_2 = 0, i;
-	char *str = dest;
+	int len_1, len_2, i;
+	char *str;
 
-	while (*(src + len_2) != '\0')
-		len_2++;
-	while (*(dest + len_1) != '\0')
-		len_1++;
+	len_1 = _strlen1(dest);
+	len_2 = _strlen1(src);
 	if (n > len_2)
 		n = len_2;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n || src[i] != '\0'; i++)
 		dest[len_1 + i] = src[i];
 	dest[len_1 + i] = '\0';
-
+	str = dest;
 	return (str);
 }
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,23 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * _strlen - Length of string
- * @s: String passed
- * Return: Length
- */
-int _strlen(char *s)
-{
-	char c = ' ';
-	int i, h;
-
-	while (c != '\0')
-	{
-		c = *(s + i);
-		h = i++;
-	}
-	return (h);
-}
 
 /**
  * _strncat - Concatenates two strings
@@ -28,34 +9,18 @@ int _strlen(char *s)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len_1, len_2, i;
-	char *str;
+	int len_1 = 0, len_2 = 0, i;
+	char *str = dest;
 
-	len_1 = _strlen(dest);
-	len_2 = _strlen(src);
+	while (*(src + len_2) != '\0')
+		len_2++;
+	while (*(dest + len_1) != '\0')
+		len_1++;
 	if (n > len_2)
 		n = len_2;
 	for (i = 0; i < n; i++)
 		dest[len_1 + i] = src[i];
 	dest[len_1 + i] = '\0';
-	str = dest;
+
 	return (str);
-}
-
-/**
- * main - Test function
- * Return: 0
- */
-int main(void)
-{
-	char *ptr;
-	char s1[98] = "Holberton ";
-	char s2[] = "School!\n";
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strncat(s1, s2, 50);
-	printf("%s\n", ptr);
-	return (0);
-
 }
