9fc23525125a239b689d3862a415c197931a0835
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-03 17:02:01 +0000

    O

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -29,23 +29,29 @@ char *cap_string(char *s)
 			}
 		}
 		else if ((s[i] == ';' || s[i] == '.') || (s[i] == '!' || s[i] == '?'))
+		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
 				str = &s[i + 1];
 				*str -= 32;
 			}
+		}
 		else if ((s[i] == '"' || s[i] == '(') || (s[i] == ')' || s[i] == '{'))
+		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
 				str = &s[i + 1];
 				*str -= 32;
 			}
+		}
 		else if (s[i] == '}')
+		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
 				str = &s[i + 1];
 				*str -= 32;
 			}
+		}
 	}
 	return (s);
 }
