a6e5c0b93f283375c60d34a02e3bda01149722ec
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-03 10:24:03 +0000

    I;

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -27,15 +27,15 @@ int _strlen(char *s)
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int len_1, len_2, i;
+	int len_1, len_2, j;
 	char *str = dest;
 
 	len_1 = _strlen(dest);
 	len_2 = _strlen(src);
 	if (n > len_2)
 		n = len_2;
-	for (i = 0; i < n || src[i] != '\0'; i++)
-		dest[len_1 + i] = src[i];
-	dest[len_1 + i] = '\0';
+	for (j = 0; j < n || src[j] != '\0'; j++)
+		dest[len_1 + j] = src[j];
+	dest[len_1 + j] = '\0';
 	return (str);
 }
