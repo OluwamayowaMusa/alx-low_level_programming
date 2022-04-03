58269ed426c02d463f39eb6bc93b2cb3434a9340
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-03 17:29:20 +0000

    Capitalize all the words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
 /**
+ * char_toupper - Change lowercase to uppercase
+ * @c: character
+ */
+void char_toupper(char *c)
+{
+	*c -= 32;
+}
+
+/**
  * cap_string - Capitalizes all words of a string
  * @s: String passed
  * Return: string
  */
 char *cap_string(char *s)
 {
-	char *str;
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
@@ -15,42 +23,27 @@ char *cap_string(char *s)
 		if (i == 0)
 		{
 			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				str = &s[i];
-				*str  -= 32;
-			}
+				char_toupper(&s[i]);
 		}
 		else if ((s[i] == ' ' || s[i] == '\t') || (s[i] == '\n' || s[i] == ','))
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				str = &s[i + 1];
-				*str -= 32;
-			}
+				char_toupper(&s[i + 1]);
 		}
 		else if ((s[i] == ';' || s[i] == '.') || (s[i] == '!' || s[i] == '?'))
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				str = &s[i + 1];
-				*str -= 32;
-			}
+				char_toupper(&s[i + 1]);
 		}
 		else if ((s[i] == '"' || s[i] == '(') || (s[i] == ')' || s[i] == '{'))
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				str = &s[i + 1];
-				*str -= 32;
-			}
+				char_toupper(&s[i + i]);
 		}
 		else if (s[i] == '}')
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				str = &s[i + 1];
-				*str -= 32;
-			}
+				char_toupper(&s[i + 1]);
 		}
 	}
 	return (s);
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,6 +1,14 @@
 #include "main.h"
-#include  <stdio.h>
+#include <stdio.h>
 
+/**
+ * char_toupper - Change lowercase to uppercase
+ * @c: character
+ */
+void char_toupper(char *c)
+{
+	*c -= 32;
+}
 
 /**
  * cap_string - Capitalizes all words of a string
@@ -9,44 +17,35 @@
  */
 char *cap_string(char *s)
 {
-	char *str;
+	int i;
 
-	for (int i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0)
 		{
 			if (s[i] >= 'a' && s[i] <= 'z')
-			{
-				str = &s[i];
-				*str  -= 32;
-			}
+				char_toupper(&s[i]);
 		}
 		else if ((s[i] == ' ' || s[i] == '\t') || (s[i] == '\n' || s[i] == ','))
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				str = &s[i + 1];
-				*str -= 32;
-			}
+				char_toupper(&s[i + 1]);
 		}
 		else if ((s[i] == ';' || s[i] == '.') || (s[i] == '!' || s[i] == '?'))
+		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				str = &s[i + 1];
-				*str -= 32;
-			}
+				char_toupper(&s[i + 1]);
+		}
 		else if ((s[i] == '"' || s[i] == '(') || (s[i] == ')' || s[i] == '{'))
+		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				str = &s[i + 1];
-				*str -= 32;
-			}
+				char_toupper(&s[i + i]);
+		}
 		else if (s[i] == '}')
+		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-			{
-				str = &s[i + 1];
-				*str -= 32;
-			}
+				char_toupper(&s[i + 1]);
+		}
 	}
 	return (s);
 }
@@ -56,10 +55,10 @@ char *cap_string(char *s)
  */
 void main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456789hello world\thello world.hello world\n";
+	char str[] = "expect the best";
 	char *ptr;
 
 	ptr = cap_string(str);
-	printf("%s", ptr);
-	printf("%s", str);
+	printf("%s\n", ptr);
+	printf("%s\n", str);
 }
