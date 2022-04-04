a4076e68dac97df43585b7c3c9b5cb8f4289228b
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-04 14:16:13 +0000

    Encode using leet code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,5 +7,23 @@
  */
 char *leet(char *s)
 {
+	char alpha[] = "aAeEoOtTlL";
+	char leet_ = "443300771";
+	char *str;
+	int i, h;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		h = 0;
+		while (h < 10)
+		{
+			if (s[i] == alpha[h])
+			{
+				str = &s[i];
+				*str = leet_[h];
+				break;
+			}
+		}
+	}
+	return (s);
 }
