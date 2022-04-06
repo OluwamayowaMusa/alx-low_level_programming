bbdc6226699c6e5ea8c3b7c91207c99e3c056fd4
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-06 14:17:36 +0000

    Learning

--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -1,45 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * _strchr - locates a character in a string
- * @s: String passed
- * @c: character passed
- *
- * Return: pointer to the first occurrence of
- * the charcter 'c' in the string 's'
- */
-char *_strchr(char *s, char c)
-{
-	char *str, st[1000000], a[98] = "nil";
-	int i, j = 0;
-
-	str = st;
-	if (c == '\0')
-		return (str);
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		{
-			for (j = 0; s[i] != '\0'; j++, i++)
-			{
-				st[j] = s[i];
-			}
-			return (str);
-		}
-	}
-	str = a;
-	return (str);
-}
-
-/**
- * main - test function
+ * main - test
  */
 void main(void)
 {
-	char *s = "hello";
-	char *f;
+	char *s = "hello world";
+	int i;
 
-	f = _strchr(s, 'l');
-	printf("%s\n", f);
+	for (i = 0; s[i] > '\0'; i++)
+	{
+		printf("%s\n", s + i);
+	}
 }
