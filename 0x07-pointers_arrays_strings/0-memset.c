6fb5f648a8486727038adfc9a361f12b21fcaf55
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-06 00:57:19 +0000

    o

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,14 +10,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char **ptr;
+	char *ptr = s;
 	unsigned int i;
 
-	ptr = &s;
 	for (i = 0; i < n; i++)
 	{
-		*ptr = &s[i];
-		**ptr = b;
+		ptr = &s[i];
+		*ptr = b;
 	}
 
 	return (ptr);
---
+++ 0x07-pointers_arrays_strings/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - test
+ */
+void main(void)
+{
+	char b = 0x01;
+
+	printf("0x%02x\n", b);
+}
