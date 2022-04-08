e8b06e87a94dcd4f1e52c427650e736cad81dc4c
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-08 22:17:46 +0000

    Understanding pointers

---
+++ pointers_arrays/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - test
+ *
+ */
+void main(void)
+{
+	int *p;
+
+	printf("Current position of p: %p\n", p);
+	printf("Current position of p + 1: %p\n", p + 1);
+}
