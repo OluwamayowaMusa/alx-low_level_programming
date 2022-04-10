bc9ef11b1c0183060cb6f1bdd79823235a058888
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-10 16:29:38 +0000

    Check prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * prime_number - Check if anumber is prime
+ * @n:Number passed
+ * @num: Test number
+ *
+ * Return: 1 - if n is prime
+ *	   0 - otherwise
+ */
+int prime_number(int n, int num)
+{
+	if (n % num == 0)
+		return (0);
+	if (num == n / 2)
+		return (1);
+	return (prime_number(n, num + 1));
+}
+
+/**
  * is_prime_number - check if a number is prime
  * @n: Number passed
  *
@@ -9,12 +26,11 @@
  */
 int is_prime_number(int n)
 {
+	int i = 2;
+
 	if (n <= 1)
 		return (0);
-	if (i > n / 2)
+	if (n == 2 || n == 3)
 		return (1);
-	if (n % i == 0)
-		return (0);
-	i++;
-	is_prime_number(n);
+	return (prime_number(n, i));
 }
--- 0x08-recursion/test_6.c
+++ 0x08-recursion/test_6.c
@@ -2,32 +2,44 @@
 #include <stdio.h>
 
 /**
+ * prime_number - Check if anumber is prime
+ * @n:Number passed
+ * @num: Test number
+ *
+ * Return: 1 - if n is prime
+ *	   0 - otherwise
+ */
+int prime_number(int n, int num)
+{
+	if (n % num == 0)
+		return (0);
+	if (num == n / 2)
+		return (1);
+	return (prime_number(n, num + 1));
+}
+
+/**
  * is_prime_number - check if a number is prime
  * @n: Number passed
  *
  * Return: 1 - if input integer is prime
  *	   0 - otherwise
  */
-int i = 2;
 int is_prime_number(int n)
 {
+	int i = 2;
+
 	if (n <= 1)
 		return (0);
-	if (i > n / 2)
+	if (n == 2 || n == 3)
 		return (1);
-	if (n % i == 0)
-		return (0);
-	i++;
-	is_prime_number(n);
+	return (prime_number(n, i));
 }
 
 /**
- * main - test fucntion
+ * main - test function
  */
 void main(void)
 {
-	int h;
-
-	h = is_prime_number(361);
-	printf("%d\n", h);
+	printf("%d\n", is_prime_number(19));
 }
