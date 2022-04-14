567f39a0d184df23e58b9e5e48c5569de5372512
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-14 08:19:37 +0000

    Create an array and initilaize it with a character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 
 	ptr = malloc(sizeof(c) * size);
-	for (i = 0; i < size; i++)
-		ptr[i] = c;
+	if (ptr == NULL)
+		return (NULL);
+	else
+		for (i = 0; i < size; i++)
+			ptr[i] = c;
 	return (ptr);
 }
