65b00909400d67fd9da83177a6800c2dc648b5a7
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-14 09:11:24 +0000

    Copies a string  to the allocated space in the memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - length of string
+ * @s: String passed
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
  * _strdup - Copies a string to an allocated space in the memory
  * @str: String  passed
  * Return: Pointer to duplicated string
@@ -14,7 +29,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	ptr = malloc(sizeof(str));
+	ptr = malloc(sizeof(char) * _strlen(str);/*len of string times size of char*/
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
--- 0x0B-malloc_free/test_1.c
+++ 0x0B-malloc_free/test_1.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 
 /**
+ * _strlen - length of string
+ * @s: string passed
+ * Return: Length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
  * _strdup - Copies a string to an allocated space in the memory
  * @str: String  passed
  * Return: Pointer to duplicated string
@@ -15,28 +29,30 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	ptr = malloc(sizeof(str));
+	ptr = malloc(sizeof(char) * _strlen(str));/*len of string times size of char*/
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; str[i]; i++)
 		ptr[i] = str[i];
 	ptr[i] = '\0';
 	return (ptr);
-	free(ptr);
 }
 
 /**
  * main - test function
+ *
+ * Return: 0
  */
-void main(void)
+int main(void)
 {
 	char *s;
 
-	s = _strdup("Holberton");
+	s = _strdup("First solve the problem, then write the code");
 	if (s == NULL)
 	{
 		printf("Failed to allocate memory\n");
 	}
 	printf("%s\n", s);
 	free(s);
+	return (0);
 }
