38caef7147301021a25bb1644f86346d507b9b2e
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-17 23:09:34 +0000

    0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,8 +19,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
-	p = (char *)ptr;
 	for (i = 0; i < nmemb; i++)
-		p[i] = NULL;
+	{
+		p = &((char *)ptr)[i];
+		*p = '\0';
+	}
 	return (ptr);
 }
