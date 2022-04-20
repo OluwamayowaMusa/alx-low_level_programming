aaccb339c37046b72e7d1eb04dabe7965929e89d
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-20 15:20:14 +0000

    O

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * new_dog - Creates a new dog
@@ -20,12 +21,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	dog_1->age = age;
+	dog_1->name = malloc(sizeof(char *));
+	if (dog_1->name == NULL)
+		return (NULL);
 	for (i = 0; name[i]; i++)
 		dog_1->name[i] = name[i];
 	dog_1->name[i] = '\0';
+	dog_1->owner = malloc(sizeof(char *));
+	if (dog_1->owner == NULL)
+		return (NULL);
 	for (i = 0; owner[i]; i++)
 		dog_1->owner[i] = owner[i];
-	dog_1->owner[i] '\0';
+	dog_1->owner[i] = '\0';
 
 	return (dog_1);
 }
