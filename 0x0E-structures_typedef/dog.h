4198f27c86f4b6af9a6b876036be6e346f767c27
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-20 13:18:26 +0000

    Structure of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Description about dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
