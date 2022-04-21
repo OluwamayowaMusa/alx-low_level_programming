d9edeb26aa0efbfdacc7c48a37790d2ad2dcd0da
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-21 04:08:41 +0000

    Prints an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Prints an array
+ * @array: Array passed
+ * @size: Size of array
+ * @action: Pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (action != NULL)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
