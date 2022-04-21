366f9f125148dcb2ea8eb8ca04b468d1831d832f
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-21 20:39:15 +0000

    Sum all the parameters using variadic functions

---
+++ 0x10-variadic_functions/test_0.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * sum_them_all - sum all the parameters
+ * @n: First parameter
+ *
+ * Return: sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+	va_list args;
+
+	va_start(args, n);
+	if ( n <= 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
+
+/**
+ * main - test function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%d\n", sum_them_all(2, 98, 1024));
+}
