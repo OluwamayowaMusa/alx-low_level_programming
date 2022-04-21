26400e6e5cb9cd5e170b0caf7eefc77bc163f714
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-21 20:50:32 +0000

    Sum all the parameters using variadic function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if ( n <= 0)
+	if (n <= 0)
 		return (0);
 	for (i = 0; i < (int)n; i++)
 		sum += va_arg(args, int);
