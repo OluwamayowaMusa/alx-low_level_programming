60a97f93a8abbd94eec21adc82d5ab9a79a92f01
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-21 21:21:10 +0000

    Print all numbers

--- 0x10-variadic_functions/test_1.c
+++ 0x10-variadic_functions/test_1.c
@@ -27,3 +27,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	putchar('\n');
 	va_end(args);
 }
+
+/**
+ * main - test function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_numbers(", ", 4, 0, 98, -1024, 402);
+	return (0);
+}
