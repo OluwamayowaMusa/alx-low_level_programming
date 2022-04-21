b5ced0f6892af8276e0c2e17f26a6aeb485f7ee2
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-21 04:17:22 +0000

    Prints an array using function pointers

--- 0x0F-function_pointers/test_1.c
+++ 0x0F-function_pointers/test_1.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * array_iterator - Prints an array
  * @array: Array passed
@@ -15,4 +15,26 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		for (i = 0; i < size; i++)
 			action(array[i]);
 	}
+}
+
+/**
+ * print_elem - prints an integer
+ * @elem: The integer to print
+ */
+void print_elem(int elem)
+{
+	printf("%d\n", elem);
+}
+
+/**
+ * main - test function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int array[5] = {0, 98, 402, 1024, 4086};
+
+	array_iterator(array, 5, &print_elem);
+	return (0);
 }
