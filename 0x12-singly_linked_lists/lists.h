3e598afd25b53c87e7e1f40f4e306f9169797afe
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-23 22:26:48 +0000

    Header File

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Header files*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/* Prototype of functions*/
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
