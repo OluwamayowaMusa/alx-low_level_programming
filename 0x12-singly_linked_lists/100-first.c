e59b088df99a24ee983c9ec60a40eed738f999e2
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-24 14:04:09 +0000

    Print message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * main - Print message to the string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house
+			upon my back!\n");
+	return (0);
+}
