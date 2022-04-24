f29682dc0442b6a2651eef25dd09210fe96ac398
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-24 18:48:09 +0000

    Run before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,6 +12,6 @@ void before_main(void)
 {
 	char *s;
 
-	s = "You're beat! and yet, you must allow,\nI bore my house upon my back";
+	s = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
 	printf("%s", s);
 }
