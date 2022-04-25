5584e73836a47bc550b522c756323d1eb9842b1b
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-25 17:40:49 +0000

    Print elements of linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+	while (h)
 	{
-		printf("%d\n", h->n);
 		n++;
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (n);
