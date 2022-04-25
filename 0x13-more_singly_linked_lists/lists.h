27b342ac95ed7a2657fc752b2c8dcdddeb0c3952
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-25 16:33:03 +0000

    Header File

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LISTS_H /*Double Guard*/
+#define LISTS_H
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: Singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
+/*Header Files*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+/*Functions Protptype*/
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+
+#endif 
