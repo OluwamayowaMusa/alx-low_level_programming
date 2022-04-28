cc87945de847b5704d96dd37bdb49422986e86a8
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-04-28 23:59:44 +0000

    Header File

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Header File */
+#include <stdlib.h>
+
+/* Function Prototypes */
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
