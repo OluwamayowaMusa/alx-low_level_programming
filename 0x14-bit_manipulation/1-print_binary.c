b4cd61d6d249b8f07bffff1ba08ada56b6f1e490
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-05-01 00:05:08 +0000

    Print number in binary representation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,52 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of number
+ * _pow - Exponent function
+ * @base: Base
+ * @exp: Exponent
+ * Return: base raised to exponent
+ */
+unsigned long int _pow(int base, int exp)
+{
+	int result = 1, i = 0;
+
+	if (exp == 0)
+		return (result);
+	while (i < exp)
+	{
+		result *= base;
+		i++;
+	}
+	return (result);
+}
+
+/**
+ * print_binary - Print binary representation of a number
  * @n: Number passed
  */
 void print_binary(unsigned long int n)
 {
-	char c;
+	unsigned long int control = 0, i = 1;
+	int temp;
 
 	if (n == 0)
-		_putchar('0');
-	while (n > 0)
 	{
-		if (n & 1 == 0)
-			_putchar(0);
-		else
-			_putchar(1);
-		n = n >> 1;
+		_putchar('0');
+		return;
 	}
-
+	while (control < n)
+	{
+		control = _pow(2, i) - 1;
+		i++;
+	}
+	control = (control + 1) >> 1;
+	while (control > 0)
+	{
+		temp = n & control;
+		if (temp == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		control = control >> 1;
+	}
 }
--- 0x14-bit_manipulation/test_1.c
+++ 0x14-bit_manipulation/test_1.c
@@ -2,25 +2,56 @@
 #include <stdio.h>
 
 /**
- * print_binary - Prints the binary representation of number
+ * _pow - Exponent function
+ * @base: Base
+ * @exp: Exponent
+ * Return: base raised to exponent
+ */
+unsigned long int _pow(int base, int exp)
+{
+	int result = 1, i = 0;
+
+	if (exp == 0)
+		return (result);
+	while (i < exp)
+	{
+		result *= base;
+		i++;
+	}
+	return (result);
+}
+
+/**
+ * print_binary - Print binary representation of a number
  * @n: Number passed
  */
 void print_binary(unsigned long int n)
 {
-	char c;
+	unsigned long int control = 0, i = 1;
+	int temp;
 
 	if (n == 0)
-		putchar('0');
-	while (n > 0)
 	{
-		if (n & 1 == 0)
-			putchar('0' + ' ');
-		else
-			putchar(('1' + ' '));
-		n = n >> 1;
+		putchar('0');
+		return;
 	}
-
+	while (control < n)
+	{
+		control = _pow(2, i) - 1;
+		i++;
+	}
+	control = (control + 1) >> 1;
+	while (control > 0)
+	{
+		temp = n & control;
+		if (temp == 0)
+			putchar('0');
+		else
+			putchar('1');
+		control = control >> 1;
+	}
 }
+
 
 /**
  * main - test function
@@ -30,14 +61,14 @@ void print_binary(unsigned long int n)
 int main(void)
 {
 	print_binary(0);
-	putchar(10);
+	printf("\n");
 	print_binary(1);
-	putchar(10);
+	printf("\n");
 	print_binary(98);
-	putchar(10);
+	printf("\n");
 	print_binary(1024);
-	putchar(10);
+	printf("\n");
 	print_binary((1 << 10) + 1);
-	putchar(10);
+	printf("\n");
 	return (0);
 }
