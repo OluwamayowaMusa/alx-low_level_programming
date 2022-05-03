21dacd2553254d2aa3e888c70689222f7c7d3a5a
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-05-03 17:57:27 +0000

    Header File

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Header File */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/* Functions Prototypes */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
