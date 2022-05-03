aadfc3129656e005b935fe3d719cf0fcf5814f28
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-05-03 19:53:31 +0000

    Reads a textfile and prints to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
  *
  * Return: The actual number of letters read and printed
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters, 00400)
 {
 	ssize_t lettersPrinted, index = 0;
 	char *text;
