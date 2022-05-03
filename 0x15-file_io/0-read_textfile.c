aefa1d36cd43c9034831496f609f26c8c47205b9
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-05-03 21:06:14 +0000

    Reads a textfile and prints to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -34,5 +34,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	free(text);
+	close(fd);
 	return (lettersPrinted);
 }
--- 0x15-file_io/test_0.c
+++ 0x15-file_io/test_0.c
@@ -34,6 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	free(text);
+	close(fd);
 	return (lettersPrinted);
 }
 
