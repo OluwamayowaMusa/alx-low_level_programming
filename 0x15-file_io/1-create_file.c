8d43f6402b9a26cddd321b470ab795aa8ab67337
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-05-04 14:07:49 +0000

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -43,6 +43,7 @@ int create_file(const char *filename, char *text_content)
 	lettersWritten = write(fd, text_content, lenOfContent);
 	if (lettersWritten == -1)
 		return (-1);
+
 	close(fd);
 	return (1);
 }
