42954eff2f7a531ec867f1e293d4b360499777b1
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-05-04 14:04:47 +0000

    O

--- 0x15-file_io/test_1.c
+++ 0x15-file_io/test_1.c
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	lenOfContent = _strlen(text_content) + 1;
+	lenOfContent = _strlen(text_content);
 	lettersWritten = write(fd, text_content, lenOfContent);
 	if (lettersWritten == -1)
 		return (-1);
