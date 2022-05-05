aa663780aa1ad5cd9fc1c7c99e021fc5b0e98bb8
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-05-05 17:43:05 +0000

    Copies the content of one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,6 +22,15 @@ void read_fail(char *s)
 	exit(98);
 }
 
+/**
+ * close_fail - Write message to standard error
+ * @i: status passed
+ */
+void close_fail(int i)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", i);
+	exit(100);
+}
 
 /**
  * copy_file - Copy file content from one file to another
@@ -35,7 +44,7 @@ int copy_file(char *filename1, char *filename2)
 {
 	ssize_t lettersRead, lettersWritten;
 	char buf[1024]; /* Buffer */
-	int fd1, fd2, fd1Close, fd2Close; /* File Descriptors */
+	int fd1, fd2, fd1Close, fd2Close, active = 1; /* File Descriptors */
 
 	if (filename1 == NULL || filename2 == NULL)
 		return (-1);
@@ -51,10 +60,13 @@ int copy_file(char *filename1, char *filename2)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename2);
 		exit(99);
 	}
-	while ((lettersRead = read(fd1, buf, 1024)) > 0)
+	while (active)
 	{
+		lettersRead = read(fd1, buf, 1024);
 		if (lettersRead == -1)
 			read_fail(filename1);
+		if (lettersRead == 0)
+			active = 0;
 		lettersWritten = write(fd2, buf, lettersRead);
 		if (lettersWritten == -1)
 			write_fail(filename2);
@@ -63,16 +75,13 @@ int copy_file(char *filename1, char *filename2)
 	fd2Close = close(fd2);
 	if (fd1Close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
+		close_fail(fd1);
 	}
 	if (fd2Close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
+		close_fail(fd2);
 	}
 	return (1);
-
 }
 
 /**
