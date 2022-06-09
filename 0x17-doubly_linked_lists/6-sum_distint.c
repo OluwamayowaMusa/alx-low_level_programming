642b2ab43a47cb78e3117711939e476916383ffb
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-09 14:43:20 +0000

    Sum of data in linked list

---
+++ 0x17-doubly_linked_lists/6-sum_distint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sum of all the data
+ * @head: Pointer to linked list
+ *
+ * Return: Sum of data in linked list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
