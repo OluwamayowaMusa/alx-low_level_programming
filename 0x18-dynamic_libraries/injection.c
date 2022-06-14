dc607115fecd00e80a17010bac13257c4fa7a0f4
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-14 12:36:34 +0000

    Test

--- 0x18-dynamic_libraries/injection.c
+++ 0x18-dynamic_libraries/injection.c
@@ -1,4 +1,4 @@
-
+int num = 0;
 /**
  * rand - random number generator
  *
@@ -6,5 +6,21 @@
  */
 int rand(void)
 {
+	num++;
+	switch (num)
+	{
+		case 1:
+			return (18);
+		case 2:
+			return (8);
+		case 3:
+			return (10);
+		case 4:
+			return (24);
+		case 5:
+			return (75);
+		case 6:
+			return (9);
+	}
 	return (22);
 }
