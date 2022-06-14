cbf691bd51f82fe58e99e32ed5d3073851daa935
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-14 13:00:26 +0000

    Inject program

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ./test/ https://github.com/OluwamayowaMusa/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injection.so
-export LD_PRELOAD=./test/injection.so
+wget -P /tmp/ https://github.com/OluwamayowaMusa/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injection.so
+export LD_PRELOAD=/tmp/injection.so
