06afbfa32cde1a8f53ed63e9e9a33159e5dcc176
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-14 12:11:59 +0000

    Test

--- 0x18-dynamic_libraries/injection.c
+++ 0x18-dynamic_libraries/injection.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-int numIterations = 0;
 
 /**
  * rand - random number generator
@@ -9,27 +8,5 @@ int numIterations = 0;
  */
 int rand(void)
 {
-	numIterations++;
-	switch (numIterations)
-	{
-		case 1:
-			printf("%d\n", numIterations);
-			return (9);
-		case 2:
-			printf("%d\n", numIterations);
-			return (8);
-		case 3:
-			printf("%d\n", numIterations);
-			return (10);
-		case 4:
-			printf("%d\n", numIterations);
-			return (24);
-		case 5:
-			printf("%d\n", numIterations);
-			return (75);
-		case 6:
-			printf("%d\n", numIterations);
-			return (9);
-	}
-	return (22);
+	return (5);
 }
