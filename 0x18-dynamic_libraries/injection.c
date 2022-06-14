091bd1d1168dc99f1f23e245c0c012ed7d197a70
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-14 11:36:51 +0000

    Test

--- 0x18-dynamic_libraries/injection.c
+++ 0x18-dynamic_libraries/injection.c
@@ -1,16 +1,5 @@
 #include <unistd.h>
 
-/**
- * puts - Write string to standard ouput
- * @s: String passed
- *
- * Return: Number of bytes written
- */
-int puts(const char *s)
-{
-	write(STDOUT_FILENO, "Congratulations, you win the Jackpot!\n", 38);
-	return (0);
-}
 
 int numIterations = 0;
 
