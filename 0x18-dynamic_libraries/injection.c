c0966c5b14816398fba81a67cf97670c73bcca47
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-14 12:58:00 +0000

    test

--- 0x18-dynamic_libraries/injection.c
+++ 0x18-dynamic_libraries/injection.c
@@ -6,8 +6,9 @@
  * @seed: seed passed
  *
  */
-void seed(unsigned int __attribute__((unused)) seed)
+void srand(unsigned int __attribute__((unused)) seed)
 {
+
 
 	write(STDOUT_FILENO, "9 8 10 24 75 - 9\n", 17);
 	write(STDOUT_FILENO, "Congratulations, you win the Jackpot!\n", 38);
