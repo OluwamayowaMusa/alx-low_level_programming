ab45b1922e0c3c311295818cba25dc01f88ef5c5
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-14 11:31:29 +0000

    Update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O tmp/injection.so https://github.com/OluwamayowaMusa/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injection.so
-export LD_PRELOAD=/tmp/injection.so
+wget -O test/injection.so https://github.com/OluwamayowaMusa/alx-low_level_programming/raw/master/0x18-dynamic_libraries/injection.so
+export LD_PRELOAD=./test/injection.so
--- 0x18-dynamic_libraries/injection.c
+++ 0x18-dynamic_libraries/injection.c
@@ -11,3 +11,31 @@ int puts(const char *s)
 	write(STDOUT_FILENO, "Congratulations, you win the Jackpot!\n", 38);
 	return (0);
 }
+
+int numIterations = 0;
+
+/**
+ * rand - generate random numbers
+ *
+ * Return: Number
+ */
+int rand()
+{
+	numIterations++;
+	switch (numIterations)
+	{
+		case 1:
+			return (9);
+		case 2:
+			return (8);
+		case 3:
+			return (10);
+		case 4:
+			return (24);
+		case 5:
+			return (75);
+		case 6:
+			return (9);
+	}
+	return (22);
+}
