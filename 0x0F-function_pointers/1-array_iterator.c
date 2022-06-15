6179438fa315d7cee598c602972bce2ea360fc00
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-15 20:56:39 +0000

    Using function Pointers on elements of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,18 @@
 #include "function_pointers.h"
 
-/**
- * array_iterator - Prints an array
- * @array: Array passed
+/**array_iterator - Executes a function on each element of an array
+ * @array: Array
  * @size: Size of array
- * @action: Pointer to function
+ * @action: Function pointers
+ *
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (action != NULL && array != NULL)
-	{
-		size_t i;
+	size_t i;
 
-		for (i = 0; i < size; i++)
+	if (action != NULL)
+	{
+		for (i = 0;  i < size; i++)
 			action(array[i]);
 	}
 }
