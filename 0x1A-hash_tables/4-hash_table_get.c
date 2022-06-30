148258b23e2f6843278e41b85c0812314baa5692
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-30 20:16:27 +0000

    Retrieves the value of a key in a hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,6 +18,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	node = (ht->array)[index];
 	if (node != NULL)
-		return (node->value);
+	{
+		while (node != NULL)
+		{
+			if (strcmp(node->key, key) == 0)
+				return (node->value);
+			node = node->next;
+		}
+	}
 	return (NULL);
 }
