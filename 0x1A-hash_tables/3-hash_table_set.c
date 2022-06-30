8a32472bbbee67eb1923c66cd9b9176309b74432
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-06-30 18:04:40 +0000

    Adding an element to an hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_hash_node = NULL, *temp = NULL;
 	unsigned long int index;
 
-	if (key == NULL || ht == NULL || strlen(key) == 0)
+	if (key == NULL || ht == NULL)
 		return (0);
 	new_hash_node = malloc(sizeof(hash_node_t));
 	if (new_hash_node == NULL)
@@ -26,10 +26,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if ((ht->array)[index] == NULL)
 	{
 		(ht->array)[index] = new_hash_node;
-		return (1);
 	}
-	temp = (ht->array)[index];
-	new_hash_node->next = temp;
-	(ht->array)[index] = new_hash_node;
+	else
+	{
+		temp = (ht->array)[index];
+		new_hash_node->next = temp;
+		(ht->array)[index] = new_hash_node;
+	}
 	return (1);
 }
