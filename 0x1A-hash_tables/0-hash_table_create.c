ba33bf0bbc8b64ebd28dc5fbd1389d065ded188b
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-07-01 19:06:11 +0000

    Test

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
+	{
+		free(new_table);
 		return (NULL);
+	}
 	for (index = 0; index < size; index++)
 		(new_table->array)[index] = NULL;
 	return (new_table);
