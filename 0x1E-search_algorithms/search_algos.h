73017355752962abbcd005dfac11ec6b92a15d9b
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-10-04 14:32:52 +0000

    Header File

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* Header Files */
+#include <stdio.h>
+
+/* Function Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+
+#endif /* SEARCH_ALGOS */
