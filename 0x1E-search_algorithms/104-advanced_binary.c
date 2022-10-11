ac8d9aaa9b658bed6ded64f8926300a5e33d285b
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-10-11 13:10:53 +0000

    Fixed Error

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -53,7 +53,7 @@ int recursive_search(int *array, size_t start, size_t end, int value)
 	else if (value > array[mid])
 		index = recursive_search(array, mid + 1, end, value);
 	else if (value < array[mid])
-		index = recursive_search(array, start, mid, value);
+		index = recursive_search(array, start, mid + 1, value);
 	return (index);
 }
 
